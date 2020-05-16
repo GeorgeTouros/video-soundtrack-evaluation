ba7768e9bf6c130e1c58736b37633412a469ea78
GTouros <tourosgeorge@gmail.com>
2020-05-16 13:13:26 +0300

    documentation additions

--- get_soundtracks.py
+++ get_soundtracks.py
@@ -25,6 +25,11 @@ def reg_cleaner(string):
 
 
 def get_film_albums(film_list=films):
+    """
+    Queries spotify with a list of films, to get albums with similar names.
+    :param film_list: a list of films
+    :return: dataframe with all the matches
+    """
     albums = pd.DataFrame()
     for film in film_list:
         result = sp.search(q=film, limit=50, type='album')
@@ -35,6 +40,11 @@ def get_film_albums(film_list=films):
 
 
 def get_film_playlists(film_list=films):
+    """
+    Queries spotify with a list of films, to get user playlists with similar names.
+    :param film_list: a list of films
+    :return: dataframe with all the matches
+    """
     playlists = pd.DataFrame()
     for film in film_list:
         result = sp.search(q=film, limit=50, type='playlist')
@@ -45,11 +55,22 @@ def get_film_playlists(film_list=films):
 
 
 def find_film_soundtrack_album(albums):
+    """
+    clean up a DataFrame of spotify albums with film OSTs 
+    :param albums: the DataFrame containing the albums
+    :return: a clean small Dataframe with the top matches for each film.
+    """
     albums = albums.loc[albums['total_tracks'] > 1]
     return albums
 
 
 def find_film_soundtrack_playlist(playlists, film_list=films):
+    """
+    clean up a DataFrame of spotify user playlists with film OSTs, given a list of films.
+    :param playlists: the DataFrame containing the user playlists.
+    :param film_list: the list of films you're searching for. Defaults on the films list at the top of the script
+    :return: a clean small Dataframe with the top match for each film.
+    """
     playlists['clean_names'] = [reg_cleaner(name) for name in playlists['name']]
     playlists['spotify_url'] = [str(url).strip('{\'spotify\': \'').strip("\'}") for url in playlists['external_urls']]
     clean_films = [reg_cleaner(film) for film in film_list]
@@ -63,6 +84,12 @@ def find_film_soundtrack_playlist(playlists, film_list=films):
 
 
 def find_songs_in_playlist(playlists):
+    """
+    Get a playlist and return a dataframe with all the tracks, complete with film name, artist, track name and
+    spotify URL.
+    :param playlists: the Dataframe with the film OST playlists.
+    :return: Dataframe
+    """
     useful = playlists[['id', 'has_film']]
     rows = pd.DataFrame()
     for playlist, film in useful.values:
