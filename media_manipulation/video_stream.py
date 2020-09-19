ba64fe1288cceee6473fe1e0cfff8fd9e2ad09fb
zappatistas20 <spag167100>
2020-09-19 13:54:26 +0300

    true_positive filtering completed

--- media_manipulation/video_stream.py
+++ media_manipulation/video_stream.py
@@ -5,6 +5,8 @@ from paths import temp_dir
 import pandas as pd
 import re
 import os
+from statistics import mode
+from statistics import StatisticsError
 
 CHUNK_SIZE = 30 * 1000  # ms
 
@@ -64,8 +66,18 @@ def find_songs_in_temp_dir():
     return songs
 
 
-def needs_smoothing(prev_chunk_label, next_chunk_label):
-    if prev_chunk_label == next_chunk_label:
+def get_previous_and_next_values(df, columns_of_interest):
+    for column in columns_of_interest:
+        if column in df.columns:
+            new_column_prev_name = 'prev_' + column
+            df[new_column_prev_name] = df[column].shift(1)
+            new_column_next_name = 'next_' + column
+            df[new_column_next_name] = df[column].shift(-1)
+    return df
+
+
+def needs_smoothing(prev_chunk_label, next_chunk_label, chunk_label):
+    if prev_chunk_label == next_chunk_label and prev_chunk_label != chunk_label:
         return True
     else:
         return False
@@ -77,19 +89,79 @@ def smooth_chunk_offset(prev_offset, next_offset):
 
 
 def smooth_chunk_matches(chunk_match_data):
+    """
+    for each row, if the previous match is the same as the next one, change the label of the row and set the offset as
+    the mean between the 2 rows.
+    :param chunk_match_data:
+    :return: smooth song label, smooth song offset
+    """
     chunk_label = chunk_match_data['song_id']
-    chunk_offset = chunk_match_data['offset']
-    prev_chunk_label = chunk_match_data['prev_song']
-    prev_offset = chunk_match_data['prev_offset']
-    next_chunk_label = chunk_match_data['next_song']
-    next_offset = chunk_match_data['next_offset']
-    if needs_smoothing(prev_chunk_label, next_chunk_label):
+    chunk_offset = chunk_match_data['offset_seconds']
+    prev_chunk_label = chunk_match_data['prev_song_id']
+    prev_offset = chunk_match_data['prev_offset_seconds']
+    next_chunk_label = chunk_match_data['next_song_id']
+    next_offset = chunk_match_data['next_offset_seconds']
+    if needs_smoothing(prev_chunk_label, next_chunk_label, chunk_label):
         return prev_chunk_label, smooth_chunk_offset(prev_offset, next_offset)
     else:
         return chunk_label, chunk_offset
 
 
-# TODO: Compare the mode of the offset_seconds column for consecutive matched chunks, with the chunk_siza
+def ieob_tagging_for_chunk_matches(chunk_match_data):
+    prev_chunk_label = chunk_match_data['prev_song_id']
+    chunk_label = chunk_match_data['song_id']
+    next_chunk_label = chunk_match_data['next_song_id']
+    if prev_chunk_label != chunk_label and chunk_label == next_chunk_label:
+        return "B"
+    if prev_chunk_label == chunk_label and chunk_label == next_chunk_label:
+        return "I"
+    if prev_chunk_label == chunk_label and chunk_label != next_chunk_label:
+        return "E"
+    else:
+        return "O"
+
+
+def calculate_offset_diff(chunk_match_data):
+    chunk_offset = chunk_match_data['offset_seconds']
+    prev_offset = chunk_match_data['prev_offset_seconds']
+    tag = chunk_match_data['match_tag']
+    if tag in ['I', 'E']:
+        offset_diff = round(chunk_offset - prev_offset, 0)
+        return offset_diff
+    else:
+        return None
+
+
+def create_match_ids(tags):
+    match_id = 0
+    match_ids = []
+    for tag in tags:
+        if tag == 'B':
+            match_id += 1
+            match_ids.append(match_id)
+        elif tag in ['I', 'E']:
+            match_ids.append(match_id)
+        else:
+            match_ids.append(None)
+    return match_ids
+
+
+def get_match_area_mode(match_area):
+    try:
+        return mode(match_area)
+    except StatisticsError:
+        return None
+
+
+def get_true_positives(df):
+    filter_df = df[df['match_ids'].notna()]
+    grouped_df = filter_df.groupby('match_ids')['offset_diff']
+    modes = grouped_df.apply(get_match_area_mode)
+    tp = modes[modes.notna()].index.values
+    tp_df = df[df['match_ids'].isin(tp)]
+    return tp_df
+
+
 # TODO: Combine the matched chunks into one and crop the video in the appropriate timestamps
 
 # these boots are made for walking should be on minute 45 of this film
@@ -108,19 +180,24 @@ if __name__ == '__main__':
     #test_file_path = r"/home/zappatistas20/Videos/Full Metal Jacket (1987)/Full.Metal.Jacket.1987.720p.BrRip.YIFY.mp4"
     #extract_audio_chunks_from_video(test_file_path, 5*1000)
     #matches = find_songs_in_temp_dir()
-    #os.chdir("../var/")
     #match_df = pd.DataFrame.from_records(matches, columns=['chunk', 'start', 'end', 'song_id',
     #                                                       'song_name', 'input_confidence', 'offset', 'offset_seconds'])
+    #match_df.sort_values(by='start', inplace=True)
+    #match_df.to_csv('../var/test_fingerprints.csv', index=False)
     match_df = pd.read_csv('../var/test_fingerprints.csv')
     match_df.sort_values(by='start', inplace=True)
-    match_df['prev_song'] = match_df['song_id'].shift(1)
-    match_df['prev_offset'] = match_df['offset'].shift(1)
-    match_df['next_song'] = match_df['song_id'].shift(-1)
-    match_df['next_offset'] = match_df['offset'].shift(-1)
-
-    match_df['smooth_song_id'], match_df['smooth_song_label'] = zip(*match_df.apply(func=smooth_chunk_matches, axis=1))
-
-    match_df.to_csv('test_fingerprints.csv', index=False)
+    match_df = get_previous_and_next_values(match_df, ['song_id', 'offset_seconds'])
+    match_df.sort_values(by='start', inplace=True)
+    match_df['song_id'], match_df['offset_seconds'] = zip(*match_df.apply(func=smooth_chunk_matches, axis=1))
+    match_df.sort_values(by='start', inplace=True)
+    match_df = get_previous_and_next_values(match_df, ['song_id', 'offset_seconds'])
+    match_df.sort_values(by='start', inplace=True)
+    match_df['match_tag'] = match_df.apply(func=ieob_tagging_for_chunk_matches, axis=1)
+    match_df.sort_values(by='start', inplace=True)
+    match_df['offset_diff'] = match_df.apply(func=calculate_offset_diff, axis=1)
+    match_df['match_ids'] = create_match_ids(match_df['match_tag'].values)
+    match_df = get_true_positives(match_df)
+    match_df.to_csv('../var/test_smoothing.csv', index=False)
     print('Done')
     # res = ask_dejavu(temp_dir+'vid_chunk__2730000__2760000.mp3')
     # res = ask_dejavu('/home/zappatistas20/PycharmProjects/thesis_dataset_creation/temp/M25067A142098_11  NANCY SINATRA - THESE BOOTS ARE MADE FOR WALKING.mp3')
